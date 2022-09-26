4022615be7d480a770e26a256fbe1d6a91a723cd
Fernando Rivera Bello <fernando.rivbe@gmail.com>
2022-09-26 12:01:20 -0500

    Delete unnecessary directory

---
+++ functions/api/Serverless.Api.Common/Constants/Routes.cs
@@ -0,0 +1,23 @@
+﻿// ***********************************************************************
+// <copyright file="Routes.cs" company="GBM">
+//     Serverless example
+// </copyright>
+// ***********************************************************************
+
+namespace Serverless.Api.Common.Constants
+{
+    /// <summary>
+    /// Constants of the routes of this service.
+    /// </summary>
+    /// <threadsafety>
+    /// Any public static (Shared in Visual Basic) members of this type are thread safe. Any instance members are not guaranteed
+    /// to be thread safe.
+    /// </threadsafety>
+    public static class Routes
+    {
+        /// <summary>
+        /// The health route.
+        /// </summary>
+        public const string Health = "/health";
+    }
+}
---
+++ functions/api/src/Serverless.Api.Controllers/HealthController.cs
@@ -0,0 +1,33 @@
+﻿// ***********************************************************************
+// <copyright file="HealthController.cs" company="GBM">
+//     Serverless example
+// </copyright>
+// ***********************************************************************
+
+using Serverless.Api.Common.Constants;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Serverless.Api.Controllers
+{
+    /// <summary>
+    /// HAndles all requests for service health
+    /// </summary>
+    [ApiController]
+    [Route(Routes.Health)]
+    public class HealthController : ControllerBase
+    {
+        /// <summary>
+        /// Adds the specified address.
+        /// </summary>
+        /// <returns>The service health check.</returns>
+        /// <response code="200">The service health check.</response>
+        /// <response code="500">An unhandled error in the server.</response>
+        [HttpGet(Name = "GetHealth")]
+        public async Task<IActionResult> GetHealthAsync()
+        {
+            var result = await Task.FromResult("healthy").ConfigureAwait(false);
+            return this.Ok(result);
+
+        }
+    }
+}
